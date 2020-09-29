9cf1bee34297b9ab20b4c1c0b8c0de9aaef88b75
Rahul Ghosh <rahulghosh096@gmail.com>
2020-09-29 22:31:47 +0600

    version 10 client part add slick jquery plugin

--- custome.js
+++ custome.js
@@ -1,12 +1,46 @@
-// Isotope plugin
+// Isotope plugin code
 // init Isotope
-var $grid = $('.portfolio_item').isotope({
-    // options
+var $grid = $(".portfolio_item").isotope({
+  // options
 });
 // filter items on button click
-$('.portfolio_menu').on('click', 'li', function () {
-    var filterValue = $(this).attr('data-filter');
-    $grid.isotope({
-        filter: filterValue
-    });
+$(".portfolio_menu").on("click", "li", function () {
+  var filterValue = $(this).attr("data-filter");
+  $grid.isotope({
+    filter: filterValue,
+  });
+});
+
+// Slick plugin Code
+
+$("document").ready(function () {
+  $(".logo-area").slick({
+    slidesToShow: 6,
+    slidesToScrool: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 6,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+    ],
+  });
 });
